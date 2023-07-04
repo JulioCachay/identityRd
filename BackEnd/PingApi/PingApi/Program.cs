28a11ee2c8313979c6987acf9b7a4dc5bb8695fb
Julio Cachay <jcachay@mktg33.com>
2023-07-04 07:48:03 -0400

    Reset

--- BackEnd/PingApi/PingApi/Program.cs
+++ BackEnd/PingApi/PingApi/Program.cs
@@ -3,6 +3,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// 1. Add Authentication Services
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.Authority = "https://dev-8is56w4e0wopjkjp.us.auth0.com/";
+    options.Audience = "https://ping-api";
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
